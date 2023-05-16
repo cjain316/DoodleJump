bba8522eb1a1de0bf2c5c4e850d921ab6cfe1074
1964037 <1964037@01-19757.pusd.dom>
2023-05-16 08:52:33 -0700

    main mostly set up

--- DoodleJump/src/Main.java
+++ DoodleJump/src/Main.java
@@ -1,9 +1,122 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.PointerInfo;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class Main {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.Timer;
+
+public class Main extends JPanel implements KeyListener, ActionListener, MouseListener {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Main f = new Main();
+	}
+	
+	public void paint(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        
+        
+        PointerInfo p = MouseInfo.getPointerInfo();
+        Point point = p.getLocation();
+        SwingUtilities.convertPointFromScreen(point, getFocusCycleRootAncestor());
+        point.setLocation(point.getX()-5,point.getY()-30);
+        
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	Timer t;
+	
+	
+	
+	public Main() {
+    	
+        JFrame f = new JFrame("Doodle Jump");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(1617,1140);
+
+        f.add(this);
+        f.addMouseListener(this);
+        f.addKeyListener(this);
+        f.setResizable(false);
+        f.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+        
+        t = new Timer(1, this);
+        t.start();
+        f.setVisible(true);
+        
+       
+        
+    }
 }
